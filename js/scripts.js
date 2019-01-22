e32f114334a634390d13b6adb430093ad628c89a
Tate Easterbrooks and Tracy Jo Reith <tracy@>
2019-01-22 10:21:50 -0800

    Add js code to pull 3 items from an array and push them into a new empty array

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,19 @@ $(document).ready(function(){
 
 var favs = [hobby, food, color, vacation, movie]
 console.log(favs.toString());
+
+var fav0 = favs[0];
+var fav1 = favs[1];
+var fav2 = favs[2];
+console.log(fav0.toString());
+console.log(fav1.toString());
+console.log(fav2.toString());
+
+
+var newFavs = [];
+newFavs.push(fav0,fav1,fav2);
+console.log(newFavs.toString());
+
  event.preventDefault();
   });
 });
